4042e5801acb8c5a50be9acd97c1bee859898c37
Adrien <forbu14@gmail.com>
2018-02-09 16:35:32 +0000

    add LRU model

--- models_one_shot.py
+++ models_one_shot.py
@@ -16,6 +16,8 @@ import torch.backends.cudnn as cudnn
 
 from NTM.NTM_fullmodel import EncapsulatedNTM
 
+from NTM_LRU.NTM_fullmodel import EncapsulatedNTM_LRU
+
 class One_shot_classifier(nn.Module):
     def __init__(self,n_output,controller_size,controller_layers,num_heads,N,M):
         super(One_shot_classifier, self).__init__()
@@ -70,6 +72,59 @@ class One_shot_classifier(nn.Module):
         result, previous_state = self.NTM_layer(aggregation)
         return result
 
+class One_shot_classifier_LRU(nn.Module):
+    def __init__(self,n_output,controller_size,controller_layers,num_heads,N,M):
+        super(One_shot_classifier_LRU, self).__init__()
+        """
+        Three components :
+            - Convolutionnal layer
+            - Neural Turing machine layer
+        """
+        self.representation_layer = nn.Sequential(
+                nn.Conv2d(1,16,kernel_size=3, padding=2),
+                nn.BatchNorm2d(16),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+                nn.Conv2d(16, 32, kernel_size=3, padding=2),
+                nn.BatchNorm2d(32),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+                nn.Conv2d(32, 64, kernel_size=3, padding=2),
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+                nn.Conv2d(64, 32, kernel_size=3, padding=2),
+                nn.BatchNorm2d(32),
+                nn.ReLU(),
+                nn.MaxPool2d(4)
+                )
+        
+        # get the dimension output
+        """
+        NTM modified layer for one shot learning :
+        """
+        num_inputs = 8*8*8 + n_output
+        
+        self.NTM_layer = EncapsulatedNTM_LRU(num_inputs, n_output,
+                 controller_size, controller_layers, num_heads, N, M)
+        
+    def forward(self, images_t,label_t_1):
+        # Input 105x105
+        represent = self.representation_layer(images_t)
+        
+        # transforming into 32 feature map of 8x8
+        represent = represent.view(-1, 8*8*8)
+        
+        # Aggregation of the representation layer and the label information
+        
+        
+        
+        aggregation = torch.cat((represent, label_t_1),1)
+        
+
+        
+        result, previous_state = self.NTM_layer(aggregation)
+        return result
 
 
  
